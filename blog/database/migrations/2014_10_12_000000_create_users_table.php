3909acf0094ac624091ac33e132152937973716b
Trung Ly <trung@tradesy.com>
2020-03-11 14:44:54 -0700

    update create_users statement

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('gender');
+            $table->string('ip_address');
             $table->rememberToken();
             $table->timestamps();
         });
