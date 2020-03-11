6a68b3fc231baf73c51f260ed4d7460613a0483e
Alvaro Valdivieso <alvaro@divie.com>
2020-03-11 14:44:09 -0700

    db seeder

--- blog/database/seeds/DatabaseSeeder.php
+++ blog/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'first_name' => Str::random(10),
+            'last_name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'gender' => 'male',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
